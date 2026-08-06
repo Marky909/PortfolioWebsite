76bd8672173ddf9a8d15ac064dd84b5b495b4d45
Marky <markdangal5858@gmail.com>
2026-08-06 14:13:52 +0545

    Initialzied git and added the the actions like about skills projects contact and also removed some of the code out of the homeController

--- PortfolioWebsite/Controllers/HomeController.cs
+++ PortfolioWebsite/Controllers/HomeController.cs
@@ -28,11 +28,5 @@ namespace PortfolioWebsite.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
